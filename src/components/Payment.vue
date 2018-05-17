6783f6f6744040e5fbdfcc378f536205682c6a28
Laís Tomaz <lais@eokoe.com>
2018-05-17 18:52:41 -0300

    Create Payment component

---
+++ src/components/Payment.vue
@@ -0,0 +1,15 @@
+<template>
+  <selectValue />
+</template>
+
+<script>
+// @ is an alias to /src
+import selectValue from '@/components/steps/selectValue.vue';
+
+export default {
+  name: 'home',
+  components: {
+    selectValue,
+  },
+};
+</script>
