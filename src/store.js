f38af9bd53fe99f6a78fabb42bfd250da1ae6066
Laís Tomaz <lais@eokoe.com>
2018-05-17 18:52:29 -0300

    Import axios and configure api variable in store

--- src/store.js
+++ src/store.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
+
+const api = 'https://dapi.votolegal.com.br/api2/';
 
 export default new Vuex.Store({
   state: {
