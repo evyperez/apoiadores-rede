dfbaf7130507bb4e6eb73121cdb0fea2d591e646
Laís Tomaz <lais@eokoe.com>
2018-05-17 18:52:09 -0300

    Add filter to Vue

--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import { formatBRL, formatDate, formatCNPJ, formatDateBasic } from './utilities';
+
 Vue.config.productionTip = false;
+
+Vue.filter('formatBRL', formatBRL);
+Vue.filter('formatDate', formatDate);
+Vue.filter('formatCNPJ', formatCNPJ);
+Vue.filter('formatDateBasic', formatDateBasic);
+Vue.filter('upperCase', value => value.toUpperCase());
 
 new Vue({
   router,
