3f8cea6a4e41b372785d7f445faf5ffb21f16b35
Laís Tomaz <lais@eokoe.com>
2018-05-17 21:09:41 -0300

    Add payment step actions and mutations

--- src/store.js
+++ src/store.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef, arrow-body-style, no-param-reassign, no-console */
 import Vue from 'vue';
 import Vuex from 'vuex';
 import axios from 'axios';
@@ -8,12 +9,24 @@ const api = 'https://dapi.votolegal.com.br/api2/';
 
 export default new Vuex.Store({
   state: {
-
+    paymentStep: 'selectValue',
+    amount: 0,
   },
   mutations: {
-
+    SET_PAYMENT_STEP(state, { data }) {
+      state.paymentStep = data.step;
+    },
+    SET_PAYMENT_AMOUNT(state, { data }) {
+      state.amount = data.amount;
+    },
   },
   actions: {
-
+    CHANGE_PAYMENT_AMOUNT({ commit }, data) {
+      commit('SET_PAYMENT_STEP', { data });
+      commit('SET_PAYMENT_AMOUNT', { data });
+    },
+    CHANGE_PAYMENT_STEP({ commit }, data) {
+      commit('SET_PAYMENT_STEP', { data });
+    },
   },
 });
