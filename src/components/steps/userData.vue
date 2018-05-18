1722935f8b69d2288daf3617e8737c869fe79e7f
Laís Tomaz <lais@eokoe.com>
2018-05-17 22:38:20 -0300

    Add surname field on userData

--- src/components/steps/userData.vue
+++ src/components/steps/userData.vue
@@ -17,6 +17,20 @@
         </div>
 
         <div
+          :class="`input-wrapper half
+          ${validation.errors.surname ? 'has-error' : ''}`"
+        >
+          <label for="surname">Sobrenome</label>
+          <input
+            type="text"
+            name="surname"
+            v-model="surname">
+          <div class="error" v-if="validation.errors.surname">
+            {{ validation.errors.surname }}
+          </div>
+        </div>
+
+        <div
           :class="`input-wrapper
           ${validation.errors.cpf ? 'has-error' : ''}`"
         >
@@ -60,52 +74,89 @@ import { validate } from '../../utilities';
 export default {
   name: 'userData',
   directives: {
-		mask,
-	},
-	data() {
-		return {
+    mask,
+  },
+  data() {
+    return {
       loading: false,
-			errorMessage: '',
-			name: '',
-			cpf: '',
-			email: '',
-			validation: {
-				errors: {},
-			},
-		};
-	},
-	methods: {
+      errorMessage: '',
+      name: '',
+      surname: '',
+      cpf: '',
+      email: '',
+      validation: {
+        errors: {},
+      },
+    };
+  },
+  computed: {
+    amount() {
+      return this.$store.state.amount;
+    },
+    iugu() {
+      return this.$store.state.iugu;
+    },
+  },
+  methods: {
     toggleLoading() {
-			this.loading = !this.loading;
-		},
-		validateForm() {
-			this.toggleLoading();
+      this.loading = !this.loading;
+    },
+    validateForm() {
+      this.toggleLoading();
 
-			const {
-				name,
-				cpf,
-				email,
-			} = this;
+      const {
+        name,
+        surname,
+        cpf,
+        email,
+      } = this;
 
-			const fields = {
-				name,
-				cpf,
-				email,
-			};
+      const fields = {
+        name,
+        surname,
+        cpf,
+        email,
+      };
 
-			const validation = validate(fields);
+      const validation = validate(fields);
 
-			if (validation.valid) {
-				this.registerUser(fields);
-			} else {
-				this.validation = validation;
-				this.toggleLoading();
-			}
-		},
-		registerUser(data) {
-      console.log(data);
-      this.$store.dispatch('CHANGE_PAYMENT_STEP', { step: 'cardData' });
-		},
-	},
+      if (validation.valid) {
+        this.registerUser(fields);
+      } else {
+        this.validation = validation;
+        this.toggleLoading();
+      }
+    },
+    registerUser(data) {
+      this.$store.dispatch('GET_TOKEN')
+        .then((res) => {
+          const payload = {
+            payment_method: 'credit_card',
+            device_authorization_token_id: res.data.device_authorization_token_id,
+            email: data.email,
+            cpf: data.cpf,
+            name: `${data.name} ${data.surname}`,
+            amount: this.amount,
+            candidate_id: 1,
+            phone: '11232323232',
+            birthdate: '1979-11-03',
+          }
+          this.$store.dispatch('GET_DONATION', payload)
+            .then((res) => {
+              const user = {
+                name: data.name,
+                surname: data.surname,
+              }
+              this.$store.dispatch('SAVE_USERNAME', user)
+              this.handleIugu();
+              this.$store.dispatch('CHANGE_PAYMENT_STEP', { step: 'cardData' });
+            });
+      });
+    },
+    handleIugu() {
+      Iugu.setAccountID(this.iugu.account_id);
+      Iugu.setTestMode(true);
+    }
+  },
 };
 </script>
