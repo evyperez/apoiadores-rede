bc7cbf4af77648409ca5c052846c6636b26d3beb
Laís Tomaz <lais@eokoe.com>
2018-05-17 22:37:41 -0300

    Create finalMessage payment step component

---
+++ src/components/steps/finalMessage.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <template v-if="message[1].type === 'msg'">
+      <h2>{{ message[1].text }}</h2>
+    </template>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'finalMessage',
+  computed: {
+    message() {
+      return this.$store.state.message;
+    },
+  },
+}
+</script>
