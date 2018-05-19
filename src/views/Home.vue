13b9d58a7b0d800e7e74c27e60102aa09ccf3c54
Laís Tomaz <lais@eokoe.com>
2018-05-19 13:43:17 -0300

    Update content on Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -49,15 +49,15 @@
           Saiba mais
         </h2>
 
-        <p>Os problemas da política nacional nascem no seu financiamento. No centro dos escândalos de corrupção recentes, as doações de empresa para a campanhas eleitorais foram proibidas pela Justiça em 2015. No palanque, os grandes e velhos partidos encenam uma  polarização que domina o debate público. Nos bastidores, porém, deixam de lado as rivalidades e fazem acordos para conseguir mais recursos e se manter no poder. Unidos, quase triplicaram o fundo partidário e criaram um fundo eleitoral bilionário. Foi assaltando os cofres públicos que eles resolveram o buraco deixado pela falta das doações das empresas. </p>
+        <p><strong>Os problemas da política nacional nascem no seu financiamento.</strong> No centro dos escândalos de corrupção recentes, as doações de empresa para a campanhas eleitorais foram proibidas pela Justiça em 2015. <strong>No palanque, os grandes e velhos partidos encenam uma  polarização que domina o debate público. Nos bastidores, porém, deixam de lado as rivalidades e fazem acordos para conseguir mais recursos e se manter no poder.</strong> Unidos, quase triplicaram o fundo partidário e criaram um fundo eleitoral bilionário. Foi assaltando os cofres públicos que eles resolveram o buraco deixado pela falta das doações das empresas. </p>
 
-        <p>A Rede Sustentabilidade é a favor da participação do dinheiro público no financiamento da política. Quando os políticos são financiados apenas pelo dinheiro privado, ficam amarrados aos interesses dos financiadores. Em 2016, a Organização para Cooperação e Desenvolvimento Econômico (OCDE) divulgou um <a href="https://read.oecd-ilibrary.org/governance/financing-democracy_9789264249455-en#page1" target="_blank">relatório</a> que aponta que os partidos políticos de 14 dos 16 países membros europeus analisados de 2007 a 2015 são majoritariamente financiadas por meio de recursos públicos. Em média, o partidos políticos nesses países foram 73% financiados com dinheiro público, e 27% com dinheiro privado. Já no Brasil, a campanha eleitoral de 2014 foi financiada com 95% de recursos privados e apenas 5% de fundos públicos, segundo o capítulo do relatório dedicado ao Brasil e escrito pelo ministro do STF José Antonio Dias Toffoli, presidente do Tribunal Superior Eleitoral (TSE) na época. O resultado desse desequilíbrio vem sendo revelado pela Polícia Federal em operação como a Lava-Jato.</p>
+        <p>A Rede Sustentabilidade é a favor da participação do dinheiro público no financiamento da política. Quando as campanhas são financiados apenas pelo dinheiro privado, os políticos podem acabar ficando amarrados aos interesses dos financiadores. O resultado desse desequilíbrio vem sendo revelado pela Polícia Federal em operação como a Lava-Jato. Em 2016, a Organização para Cooperação e Desenvolvimento Econômico (OCDE) divulgou um <a href="https://read.oecd-ilibrary.org/governance/financing-democracy_9789264249455-en#page1" target="_blank">relatório</a> mostrando que o partidos políticos nos países pesquisados foram financiados, em média, 73% com dinheiro público e 27% com dinheiro privado. Já no Brasil, o mesmo relatório aponta que a campanha eleitoral de 2014 foi financiada com 95% de recursos privados e apenas 5% de fundos públicos. </p>
 
-        <p>O dinheiro público deve ajudar a financiar as campanhas justamente para reduzir a possibilidade de uma troca de favores com o capital privado depois das eleições. Da forma  como foi aprovada pelo Congresso, no entanto, só serve para beneficiar os mesmo partidos e engessar a política. As eleições não podem ser tão caras e os recursos não podem ser tão mal distribuídos. Temos assim um ciclo vicioso que favorece imensamente quem já está no poder.</p>
+        <p>O dinheiro público deve ajudar a financiar as campanhas justamente para reduzir a possibilidade de uma troca de favores depois das eleições. Mas não da maneira como foi aprovada pelo Congresso. O formato atual só serve para beneficiar os mesmos partidos e engessar a política. <strong>O fundo precisa ser menor e mais bem distribuído.</strong> Os grandes partidos criaram um ciclo vicioso que favorece imensamente quem já está no poder.</p>
 
-        <p>A Rede vem crescendo nos último anos e em 2018 acreditamos que temos chances reais de romper com essa lógica. Queremos concentrar esforços em expandir a presença e o alcance da Rede pelo Brasil e assim articular todas e todos que compartilham desse objetivo comum. E pra isso, nada melhor do que Marina Silva levar nossas ideias pelos quatro cantos do país, conversando olhos nos olhos com brasileiras e brasileiros e mostrando não apenas que existem novas formas de se fazer política, mas também que é possível começarmos agora!</p>
+        <p><strong>Acreditamos que temos chances reais de romper com essa lógica em 2018.</strong> Queremos concentrar esforços em expandir a presença e o alcance da Rede pelo Brasil e assim articular todas e todos que compartilham desse objetivo comum. <strong>E pra isso, nada melhor do que Marina Silva levar nossas ideias pelos quatro cantos do país,</strong> conversando olhos nos olhos com brasileiras e brasileiros e mostrando não apenas que existem novas formas de se fazer política, mas também que é possível começarmos agora!</p>
 
-        <p>Mais uma vez acreditamos na colaboração como caminho. Para reinventar o Brasil é preciso reinventar a forma como financiamos a política. Contribua e faça parte da expansão da rede que vai unir o Brasil.</p>
+        <p><strong>Para reinventar o país é preciso reinventar a forma como financiamos a política.</strong> Mais uma vez acreditamos na colaboração como caminho. <strong>Contribua e faça parte da expansão da rede que vai unir o Brasil.</strong></p>
       </div>
     </article>
 
@@ -67,7 +67,12 @@
           Metas
         </h2>
 
-        <p>Quanto queremos arrecadar e para que?</p>
+        <h3>R$ 100 mil</h3>
+        <p>Para que possamos realizar tudo que desejamos, precisamos financiar as viagens da Marina e outras lideranças da Rede pelo país. Com esse valor, conseguiremos realizar eventos em cinco capitais diferentes, levando ideias, propostas e uma visão diferente de país para diversas regiões</p>
+
+        <h3>R$ 200 mil</h3>
+        <p>Sabemos que os adversários políticos usarão de todas as artimanhas para desestabilizar a Rede e a Marina. São os reis das mentiras, acusações levianas e notícias falsas. Precisamos combater isso tudo. Com essa arrecadação produziremos vídeos e materiais gráficos abordando essas fake news contra a Marina e a Rede e divulgando as verdadeiras propostas para unir o Brasil</p>
+
       </div>
     </article>
 
