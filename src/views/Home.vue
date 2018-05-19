ee27020829f92d726a2a20061b5ef7a9c8c2ad07
Laís Tomaz <lais@eokoe.com>
2018-05-19 13:30:57 -0300

    Update content on Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,9 +3,10 @@
 		<article class="home__intro">
 			<div class="container">
         <h2>
-          Ajude a Rede e Marina a divulgarem ideias transformadoras Brasil afora
+          Expandir a Rede para unir o Brasil
         </h2>
-        <p>Nunc non euismod exx. Phasellus condimentum ipsum leo, vitae commodo diam venenatis a. Donec et est imperdiet, auctor arcu interdum, maximus eros. Morbi sollicitudin vitae diam vitae cursus. Suspendisse lectus nulla, finibus a luctus non, fringilla sit amet sem. Nam non eros quis nibh mollis laoreet vel quis sem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+
+        <p>A Rede Sustentabilidade nasce da união de pessoas que acreditam em novas formas de fazer política. Na contramão deste desejo, os grandes partidos dividem o país numa falsa polarização, mas se unem por trás dos panos para criar fundos bilionários com recursos públicos, sufocar qualquer possibilidade de renovação e se manter no poder. Com Marina como porta-voz e milhares de apoiadores em todo país, temos chances reais de fazer frente a essa farsa. Juntos, podemos equilibrar a disputa. Criamos essa campanha de financiamento coletivo para expandir a Rede e assim unir todas as pessoas que compartilham desse sonho. Uma nova forma de fazer política nasce de uma forma diferente de financiar a política. Faça sua contribuição, compartilhe a página e seja parte da mudança.</p>
 
         <section id="campaign-progress" class="campaign-progress">
           <p>
@@ -48,11 +49,15 @@
           Saiba mais
         </h2>
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tempus pulvinar massa sodales convallis. Nam massa nisl, bibendum ut justo id, vulputate auctor eros. Ut accumsan orci sem, sit amet elementum odio posuere at. Nam ac semper lectus. Quisque et velit turpis. Ut aliquam, nisi quis interdum ultrices, erat erat vehicula dui, eu commodo est ante at mi. Pellentesque vestibulum varius lacus id ultrices. Praesent egestas quam sit amet molestie accumsan. Nunc pulvinar condimentum ex non lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin accumsan risus a urna condimentum cursus. Vestibulum arcu sapien, bibendum non purus ut, sodales consectetur ipsum.</p>
+        <p>Os problemas da política nacional nascem no seu financiamento. No centro dos escândalos de corrupção recentes, as doações de empresa para a campanhas eleitorais foram proibidas pela Justiça em 2015. No palanque, os grandes e velhos partidos encenam uma  polarização que domina o debate público. Nos bastidores, porém, deixam de lado as rivalidades e fazem acordos para conseguir mais recursos e se manter no poder. Unidos, quase triplicaram o fundo partidário e criaram um fundo eleitoral bilionário. Foi assaltando os cofres públicos que eles resolveram o buraco deixado pela falta das doações das empresas. </p>
 
-        <p>Nam fermentum massa eget lorem posuere sodales. Etiam risus turpis, mollis nec efficitur in, eleifend eget arcu. Quisque libero nisl, facilisis non viverra sit amet, semper a quam. Aenean facilisis blandit imperdiet. Mauris nibh nisi, laoreet id eros in, bibendum efficitur sem. Nulla felis ipsum, tristique vitae gravida vitae, dictum at enim. Phasellus gravida sem nec consequat ultrices. Ut eu sem in nulla aliquet consectetur quis a odio. Praesent viverra nibh ex, vitae mattis sapien gravida eget. Aenean vel volutpat leo. Sed sit amet massa in lacus mollis faucibus in et nulla. Praesent quis fermentum mauris, quis volutpat nunc.</p>
+        <p>A Rede Sustentabilidade é a favor da participação do dinheiro público no financiamento da política. Quando os políticos são financiados apenas pelo dinheiro privado, ficam amarrados aos interesses dos financiadores. Em 2016, a Organização para Cooperação e Desenvolvimento Econômico (OCDE) divulgou um <a href="https://read.oecd-ilibrary.org/governance/financing-democracy_9789264249455-en#page1" target="_blank">relatório</a> que aponta que os partidos políticos de 14 dos 16 países membros europeus analisados de 2007 a 2015 são majoritariamente financiadas por meio de recursos públicos. Em média, o partidos políticos nesses países foram 73% financiados com dinheiro público, e 27% com dinheiro privado. Já no Brasil, a campanha eleitoral de 2014 foi financiada com 95% de recursos privados e apenas 5% de fundos públicos, segundo o capítulo do relatório dedicado ao Brasil e escrito pelo ministro do STF José Antonio Dias Toffoli, presidente do Tribunal Superior Eleitoral (TSE) na época. O resultado desse desequilíbrio vem sendo revelado pela Polícia Federal em operação como a Lava-Jato.</p>
 
-        <p>Donec erat magna, porta eget aliquam et, sagittis sed lacus. Vestibulum varius elit nec diam vestibulum, a commodo ipsum egestas. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis quis sagittis lectus. Morbi quis scelerisque orci. Proin sagittis ex id augue laoreet, et lobortis nibh venenatis. Donec tellus magna, tristique vel massa in, ultrices bibendum nibh. Nam eleifend congue purus non consequat. In hac habitasse platea dictumst. Maecenas ultricies mi non efficitur finibus.</p>
+        <p>O dinheiro público deve ajudar a financiar as campanhas justamente para reduzir a possibilidade de uma troca de favores com o capital privado depois das eleições. Da forma  como foi aprovada pelo Congresso, no entanto, só serve para beneficiar os mesmo partidos e engessar a política. As eleições não podem ser tão caras e os recursos não podem ser tão mal distribuídos. Temos assim um ciclo vicioso que favorece imensamente quem já está no poder.</p>
+
+        <p>A Rede vem crescendo nos último anos e em 2018 acreditamos que temos chances reais de romper com essa lógica. Queremos concentrar esforços em expandir a presença e o alcance da Rede pelo Brasil e assim articular todas e todos que compartilham desse objetivo comum. E pra isso, nada melhor do que Marina Silva levar nossas ideias pelos quatro cantos do país, conversando olhos nos olhos com brasileiras e brasileiros e mostrando não apenas que existem novas formas de se fazer política, mas também que é possível começarmos agora!</p>
+
+        <p>Mais uma vez acreditamos na colaboração como caminho. Para reinventar o Brasil é preciso reinventar a forma como financiamos a política. Contribua e faça parte da expansão da rede que vai unir o Brasil.</p>
       </div>
     </article>
 
@@ -62,7 +67,7 @@
           Metas
         </h2>
 
-        <p>Mauris porta rhoncus mi, sit amet viverra magna. Aliquam auctor nisl in lorem mollis, vel consequat dolor congue. Morbi eget mi ligula. Quisque ut leo rutrum, placerat sapien quis, accumsan tortor. Suspendisse et ipsum a tortor pharetra pretium vel ac ipsum. Mauris suscipit massa id purus accumsan elementum. Aenean mauris augue, maximus tristique ullamcorper quis, euismod in urna. Nam varius neque et ante scelerisque, sit amet mollis augue finibus. Nam lacinia molestie tellus in dapibus. Suspendisse elementum sollicitudin pretium. Maecenas nec magna at velit imperdiet auctor. Phasellus a gravida nisl, eget bibendum leo.</p>
+        <p>Quanto queremos arrecadar e para que?</p>
       </div>
     </article>
 
@@ -72,7 +77,7 @@
           Doadores
         </h2>
 
-        <p>Essas são as pessoas que entenderam o valor de seu apoio e decidiram dar um pasos na dire;cão de uma política mais transparente, mais representativa e mais colaborativa:</p>
+        <p>Essas são as pessoas que entenderam o valor de seu apoio e decidiram dar um pasos na direção de uma política mais transparente, mais representativa e mais colaborativa:</p>
         <p>
           <span v-for="(person, i) in donations" :key="person.id">
             {{ person.name }}{{ i < donations.length -1 ? ',' : '' }}
