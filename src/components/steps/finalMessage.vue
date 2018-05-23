0cb97d68ad3b73a50fcd6eeb1afcd7ea20273a76
evyperez <evelyn@eokoe.com>
2018-05-23 17:22:44 -0300

    Update `finalMessage.vue`

--- src/components/steps/finalMessage.vue
+++ src/components/steps/finalMessage.vue
@@ -1,6 +1,17 @@
 <template>
   <section>
     <h2 v-for="(message, i) in messages" :key="i">{{ message.text }}</h2>
+
+
+	<!-- <ul id="wrap-share">
+		<li>
+			<a href="https://twitter.com/appcivico" target="_blank" class="twitter"></a>
+		</li>
+		<li>
+			<a href="https://www.facebook.com/AppCivicoCSB" target="_blank" class="facebook"></a>
+		</li>
+	</ul> -->
+
   </section>
 </template>
 
