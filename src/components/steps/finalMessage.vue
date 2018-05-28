348b5316e40adf1688c1af76bae9d5f061fa4cbd
Evelyn Perez <evelyn@eokoe.com>
2018-05-28 11:02:43 -0300

    Update twitter share message

--- src/components/steps/finalMessage.vue
+++ src/components/steps/finalMessage.vue
@@ -2,10 +2,9 @@
   <section>
     <h2 v-for="(message, i) in messages" :key="i">{{ message.text }}</h2>
 
-
 	<ul id="wrap-share">
 		<li>
-			<a class="twitter-hashtag-button twitter" href="https://twitter.com/intent/tweet?button_hashtag=SomosRede">Compartilhar</a>
+			<a class="twitter-hashtag-button twitter" href="https://twitter.com/intent/tweet?button_hashtag=SomosRede%20https://somosrede.com.br/">Compartilhar</a>
 		</li>
 		<li>
 			<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fsomosrede.com.br%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore facebook">Compartilhar</a>
