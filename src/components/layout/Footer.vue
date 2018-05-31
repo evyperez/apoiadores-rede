42e3376197c04f31faa5ae5595e0f0cd67f22792
Evelyn Perez <evelyn@eokoe.com>
2018-05-31 19:00:28 -0300

    Update footer

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,16 +1,18 @@
 <template>
   <footer class="site-footer">
-    <div class="container">
-      <div class="rede-sustentabilidade">
-        <a href="http://redesustentabilidade.org.br/" target="_blank">Rede sustentabilidade</a>
-      </div>
+    <div class="footer-container">
       <div class="site-footer__copyright">
-        <div>Proporcionado por</div>
-        <ul>
-          <li class="apoiadores"><a href="https://appcivico.com/pt/apoiadores/" target="_blank">Apoiadores</a></li>
-          <li class="appcivico"><a href="https://appcivico.com/" target="_blank">AppCívico</a></li>
-        </ul>
-        <p>© 2016-2018 Tecnologia <a href="https://votolegal.com.br/" target="_blank">VotoLegal</a> • Uma iniciativa <a href="https://appcivico.com/" target="_blank">AppCívico</a></p>
+		<div class="wrap-logo">
+			<a href="https://votolegal.com.br" target="_blank" class="votolegal-logo"></a>
+		</div>
+		<div class="site-footer__copyright">
+			<p>
+				Voto Legal é uma plataforma aberta e baseada em software livre. Os realizadores não se responsabilizam pelas informações fornecidas pelos candidatos, nem pelo comportamento deles durante o período eleitoral, e se eleitos, durante seus respectivos mandatos. VotoLegal opera sob o CNPJ 30.217.474/0001-50, razão social Pague Junto - Tecnologia de Intermediação LTDA. Este projeto é distribuído sob a licença <strong><a href="https://www.gnu.org/licenses/agpl-3.0.en.html" target="_blank" >Affero General Public License</a></strong>.
+			</p>
+			<p>
+				<strong>© 2016-2018 VotoLegal • Uma iniciativa AppCívico • <a href="/termos.pdf" target="_blank">Contrato</a></strong>
+			</p>
+		</div>
       </div>
     </div>
   </footer>
