93c3666f1db5842d59d225cd8d4a8c43acb41296
Laís Tomaz <lais@eokoe.com>
2018-06-15 15:54:33 -0300

    Import components from VL

---
+++ src/components/steps/addressData.vue
@@ -0,0 +1,213 @@
+<template>
+		<form @submit.prevent="validateForm" :aria-busy="loading ? 'true' : 'false'">
+			<fieldset>
+				<div :class="`input-wrapper
+					${validation.errors.birthdate ? 'has-error' : ''}`">
+					<label for="birthdate">{{ 'birthdate' | translate }}</label>
+					<input type="text" v-model="birthdate" name="birthdate" v-mask="'##/##/####'">
+					<div class="error" v-if="validation.errors.birthdate">
+						{{ validation.errors.birthdate }}
+					</div>
+				</div>
+				<div :class="`input-wrapper
+					${validation.errors.phone ? 'has-error' : ''}`">
+					<label for="phone">{{ 'phone' | translate }}</label>
+					<input type="text" v-model="phone" name="phone" v-mask="'(##)#####-####'">
+					<div class="error" v-if="validation.errors.phone">
+						{{ validation.errors.phone }}
+					</div>
+				</div>
+				<div :class="`input-wrapper
+					${validation.errors.zip_code ? 'has-error' : ''}`">
+					<label for="zip_code">{{ 'cep' | translate }}</label>
+					<input type="text" v-model="zip_code" name="zipcode" v-mask="'#####-###'" @blur="searchAddress($event)" >
+					<div class="error" v-if="validation.errors.zip_code">
+						{{ validation.errors.zip_code }}
+					</div>
+				</div>
+				<div :class="`input-wrapper
+					${validation.errors.city ? 'has-error' : ''}`">
+					<label for="city">{{ 'city' | translate }}</label>
+					<input type="text" v-model="city" name="city" :disabled="true">
+					<div class="error" v-if="validation.errors.city">
+						{{ validation.errors.city }}
+					</div>
+				</div>
+				<div :class="`input-wrapper
+					${validation.errors.street ? 'has-error' : ''}`">
+					<label for="street">{{ 'street' | translate }}</label>
+					<input type="text" v-model="street" name="street" :disabled="true">
+					<div class="error" v-if="validation.errors.street">
+						{{ validation.errors.street }}
+					</div>
+				</div>
+				<div :class="`input-wrapper
+					${validation.errors.district ? 'has-error' : ''}`">
+					<label for="district">{{ 'district' | translate }}</label>
+					<input type="text" v-model="district" name="district" :disabled="true">
+					<div class="error" v-if="validation.errors.district">
+						{{ validation.errors.district }}
+					</div>
+				</div>
+				<div :class="`input-wrapper
+					${validation.errors.number ? 'has-error' : ''}`">
+					<label for="number">{{ 'number' | translate }}</label>
+					<input type="text" v-model="number" name="number" >
+					<div class="error" v-if="validation.errors.number" >
+						{{ validation.errors.number }}
+					</div>
+				</div>
+					<div :class="`input-wrapper
+					${validation.errors.complement ? 'has-error' : ''}`">
+					<label for="complement">{{ 'complement' | translate }}</label>
+					<input type="text" v-model="complement" name="complement" >
+					<div class="error" v-if="validation.errors.complement">
+						{{ validation.errors.complement }}
+					</div>
+				</div>
+			</fieldset>
+				<p class="error" v-if="errorMessage != ''">
+				{{ errorMessage }}
+			</p>
+			<button type="submit" :disabled="loading" class="donation-nav donation-nav--forward">Continuar</button>
+		</form>
+</template>
+
+<script>
+	import {
+		mask
+	} from 'vue-the-mask';
+	import {
+		validate,
+		removeAccented
+	} from '../../utilities';
+
+	export default {
+		name: 'adressData',
+		directives: {
+			mask,
+		},
+		data() {
+			return {
+				loading: false,
+				errorMessage: '',
+
+				zip_code: '',
+				state: '',
+				city: '',
+				street: '',
+				district: '',
+				number: '',
+				complement: '',
+				phone: '',
+				birthdate: '',
+
+				validation: {
+					errors: {},
+				},
+			};
+		},
+		computed: {
+			username() {
+				return this.$store.state.username;
+			},
+			getUserData() {
+				return this.$store.state.userData;
+			}
+		},
+		methods: {
+			toggleLoading() {
+				this.loading = !this.loading;
+			},
+			validateForm() {
+				this.toggleLoading();
+				let birthdate = this.birthdate.split("/");
+				 birthdate.reverse();
+				 birthdate = birthdate.join("-");
+
+				const address = {
+					zip_code: this.zip_code,
+					state: this.state,
+					city: this.city,
+					street: this.street,
+					district: this.district,
+					number: this.number,
+					phone: this.phone.replace(/[^\d]+/g, ""),
+					birthdate,
+				};
+
+				const validation = validate(address);
+
+				if (validation.valid) {
+					this.saveAddress()
+
+				} else {
+					this.validation = validation;
+					this.toggleLoading();
+				}
+			},
+			saveAddress(){
+
+				let birthdate = this.birthdate.split("/");
+				birthdate.reverse();
+				birthdate = birthdate.join("-");
+
+				const payload = {
+					billing_address_zipcode: this.zip_code,
+					billing_address_state: this.state,
+					billing_address_city: this.city,
+					billing_address_street: this.street,
+					billing_address_district: this.district,
+					billing_address_house_number: this.number,
+					phone: this.phone.replace(/[^\d]+/g, ""),
+					birthdate,
+					billing_address_complement: this.complement,
+
+					payment_method: this.getUserData.payment_method,
+					device_authorization_token_id: this.getUserData.device_authorization_token_id,
+					email: this.getUserData.email,
+					cpf: this.getUserData.cpf,
+					name: this.getUserData.name,
+					amount: this.getUserData.amount,
+					candidate_id: this.getUserData.candidate_id,
+					donation_fp: this.getUserData.donation_fp
+				}
+
+				 this.$store.dispatch("GET_DONATION", payload)
+					.then(res => {
+					 this.$store.dispatch("CHANGE_PAYMENT_STEP", { step: "certFaceVerify" });
+
+					})
+					.catch(err => {
+						this.toggleLoading();
+						this.handleErrorMessage(err);
+					});
+			},
+			searchAddress(event){
+				this.toggleLoading();
+				this.$store.dispatch('getAddress', event.target.value).then((response) => {
+					this.state = ( response.state  == '' ) ? this.disableField('state') : response.state;
+					this.city = ( response.city  == '' ) ? this.disableField('city') : response.city;
+					this.street = ( response.street == '' ) ? this.disableField('street') : response.street;
+					this.district = ( response.district == '' ) ? this.disableField('district') : response.district;
+					this.toggleLoading();
+					this.errorMessage = '';
+				}).catch((erro)=>{
+					this.toggleLoading();
+					this.errorMessage = 'Cep não encontrado';
+				});
+			},
+			disableField(field) {
+				 this.$nextTick(function () {
+					var element  = document.getElementsByName(field);
+					element[0].disabled=false;
+					return '';
+					})
+
+			},
+			handleErrorMessage(err) {
+				this.errorMessage = err.data[0].message;
+			},
+		},
+	};
+</script>
---
+++ src/components/steps/certFaceVerify.vue
@@ -0,0 +1,42 @@
+<template>
+		<section class="certiface-verify">
+			<div v-html="messages[0].text" ></div>
+
+			<a :href="messages[1].href" target="_blank" class="donation-nav donation-nav--forward">{{messages[1].text}}</a>
+
+		</section>
+</template>
+
+<script>
+
+	export default {
+		name: 'certFaceVerify',
+		data() {
+			return {
+				loading: false,
+				errorMessage: '',
+
+				validation: {
+					errors: {},
+				},
+			};
+		},
+		computed: {
+			username() {
+				return this.$store.state.username;
+			},
+			messages() {
+				return this.$store.state.messages;
+			},
+		},
+		methods: {
+			toggleLoading() {
+				this.loading = !this.loading;
+			},
+
+			handleErrorMessage(err) {
+				this.errorMessage = err.data[0].message;
+			},
+		},
+	};
+</script>
