5661f1fffba2183610608a2aad3e21b6afec55db
Laís Tomaz <lais@eokoe.com>
2018-06-15 16:30:17 -0300

    Make lint happy in printBoleto

--- src/components/steps/printBoleto.vue
+++ src/components/steps/printBoleto.vue
@@ -1,53 +1,53 @@
 <template>
-		<section v-if="messages" class="certiface-verify">
-			<div class="input-wrapper" v-html="messages[0].text"></div>
-			<div class="input-wrapper" v-if="messages[1]">
-				<a target="_blank" class="donation-nav donation-nav--forward" @click.prevent="redirectUser(messages[1])" >{{messages[1].text}} </a>
-			</div>
-		</section>
+    <section v-if="messages" class="certiface-verify">
+      <div class="input-wrapper" v-html="messages[0].text"></div>
+      <div class="input-wrapper" v-if="messages[1]">
+        <a target="_blank" class="donation-nav donation-nav--forward" @click.prevent="redirectUser(messages[1])" >{{ messages[1].text }} </a>
+      </div>
+    </section>
 </template>
 
 <script>
 
-	export default {
-		name: 'printBoleto',
-		data() {
-			return {
-				loading: false,
-				errorMessage: '',
+  export default {
+    name: 'printBoleto',
+    data() {
+      return {
+        loading: false,
+        errorMessage: '',
 
-				validation: {
-					errors: {},
-				},
-			};
-		},
-		computed: {
-			username() {
-				return this.$store.state.username;
-			},
-			messages() {
-				return this.$store.state.messages.messages;
-			},
-		},
-		methods: {
-			toggleLoading() {
-				this.loading = !this.loading;
-			},
-			handleErrorMessage(err) {
-				this.errorMessage = err.data[0].message;
-			},
-			redirectUser(ui) {
-				if( ui.value === "pay_with_cc" ){
-					this.$router.replace(this.$route.path);
-						this.$store.dispatch("CHANGE_PAYMENT_STEP", {
-							step: "userData"
-					});
-				}else{
-					sessionStorage.clear();
-					window.open(ui.href)
-				}
-			}
-		},
+        validation: {
+          errors: {},
+        },
+      };
+    },
+    computed: {
+      username() {
+        return this.$store.state.username;
+      },
+      messages() {
+        return this.$store.state.messages.messages;
+      },
+    },
+    methods: {
+      toggleLoading() {
+        this.loading = !this.loading;
+      },
+      handleErrorMessage(err) {
+        this.errorMessage = err.data[0].message;
+      },
+      redirectUser(ui) {
+        if( ui.value === 'pay_with_cc' ){
+          this.$router.replace(this.$route.path);
+            this.$store.dispatch('CHANGE_PAYMENT_STEP', {
+              step: 'userData'
+          });
+        }else{
+          sessionStorage.clear();
+          window.open(ui.href)
+        }
+      }
+    },
 
-	};
+  };
 </script>
