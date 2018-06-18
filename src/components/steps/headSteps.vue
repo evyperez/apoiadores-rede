3b51d7c1729a7f44f7475280909605c21ad2cfea
Laís Tomaz <lais@eokoe.com>
2018-06-18 15:20:32 -0300

    Create headSteps component

---
+++ src/components/steps/headSteps.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <ul class="head-steps">
+      <li :class="paymentStep === 'userData' ? 'active' : ''">
+        Escolha a forma de pagamento
+      </li>
+      <li :class="paymentStep === 'cardData' ? 'active' : ''">
+        Dados de cartão de crédito
+      </li>
+      <li :class="paymentStep === 'boleto' ? 'active' : ''">
+        Endereço
+      </li>
+      <li :class="paymentStep === 'certFaceVerify' ? 'active' : ''">
+        Verificação de segurança
+      </li>
+      <li :class="paymentStep === 'printBoleto' ? 'active' : ''">
+        Boleto
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'headSteps',
+  computed: {
+    paymentStep() {
+      return this.$store.state.paymentStep;
+    },
+    amount() {
+      return this.$store.state.amount;
+    },
+  },
+};
+</script>
+
