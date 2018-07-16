00c5837cefc53d1c1a8ebb88029bc6d9824ab5e7
edu ruiz <eduardo.ruiz@eokoe.com>
2018-07-16 20:17:56 -0300

    Change footer text and links

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -6,11 +6,14 @@
 			<a href="https://votolegal.com.br" target="_blank" class="votolegal-logo"></a>
 		</div>
 		<div class="site-footer__copyright">
+      <p>
+        <a href="https://votolegal.com.br">Voto Legal</a> é uma plataforma de software livre homologada pelo TSE e opera sob o CNPJ "30.217.474/0001-50", razão social "Pague Junto - Tecnologia de Intermediação LTDA".
+      </p>
+      <p>
+        O VotoLegal registra todas as doações na blockchain da Decred com objetivo de garantir a autenticidade das transações, promovendo a confiabilidade no processo de doação eleitoral.
+      </p>
 			<p>
-				<a href="https://votolegal.com.br">Voto Legal</a> é uma plataforma aberta e baseada em software livre. Os realizadores não se responsabilizam pelas informações fornecidas pelos candidatos, nem pelo comportamento deles durante o período eleitoral, e se eleitos, durante seus respectivos mandatos. <a href="https://votolegal.com.br">Voto Legal</a> opera sob o CNPJ 30.217.474/0001-50, razão social Pague Junto - Tecnologia de Intermediação LTDA. Este projeto é distribuído sob a licença <strong><a href="https://www.gnu.org/licenses/agpl-3.0.en.html" target="_blank" >Affero General Public License</a></strong>.
-			</p>
-			<p>
-				<strong>© 2016-2018 <a href="https://votolegal.com.br">Voto Legal</a> • Uma iniciativa AppCívico • <a href="/termos.pdf" target="_blank">Contrato</a></strong>
+				<strong>© 2016-2018 <a href="https://votolegal.com.br">Voto Legal</a> • Uma iniciativa <a href="http://www.appcivico.com/">AppCívico</a> - Tecnologias Cívicas • <a href="/termos.pdf" target="_blank">Contrato</a></strong>
 			</p>
 		</div>
       </div>
