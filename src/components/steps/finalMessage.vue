0e5cee18b71cb1915bfe582a393cfa4dfe7c4a65
Evelyn Perez <evelyn@eokoe.com>
2018-07-16 12:21:49 -0300

    Fix credit card final message

--- src/components/steps/finalMessage.vue
+++ src/components/steps/finalMessage.vue
@@ -1,7 +1,8 @@
 <template>
   <section>
   <a class="donation-nav donation-nav--rewind" href="#" @click.prevent="goBack">voltar</a>
-  <div v-for="(message, i) in messages" :key="i" v-html="message.text"></div>
+  <div class="input-wrapper" v-html="messages[0].text"></div>
+  <p class="input-wrapper" v-html="messages[1].text"></p>
   <ul id="wrap-share">
     <li>
       <a class="twitter" href="https://twitter.com/intent/tweet?text=Marina%20precisa%20de%20n%C3%B3s%20para%20equilibrar%20a%20disputa%21%20https%3A%2F%2Fdoemarina.com.br">Compartilhar</a>
@@ -10,8 +11,8 @@
       <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdoemarina.com.br%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore facebook">Compartilhar</a>
     </li>
   </ul>
-  <div class="donation-nav-wrap">
-  	<a class="donation-nav donation-nav--forward" href="https://eokoe2.typeform.com/to/iyaBER" target="_blank">responda nossa pesquisa</a>
+  <div class="donation-nav-wrap" v-if="messages[2]">
+    <a target="_blank" class="donation-nav donation-nav--forward" @click.prevent="redirectUser(messages[2])">{{ messages[2].text }} </a>
   </div>
   </section>
 </template>
