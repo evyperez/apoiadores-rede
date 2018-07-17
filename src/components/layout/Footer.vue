35b4f522e5113ed744983b74597f0cacb08e85b6
Evelyn Perez <evelyn@eokoe.com>
2018-07-17 14:23:51 -0300

    Add facebook chat

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,19 +1,24 @@
 <template>
   <footer class="site-footer">
+    <div class="fb-customerchat"
+      attribution=setup_tool
+      page_id="257874391684846"
+      theme_color="#0a6661">
+    </div>
     <div class="footer-container">
       <div class="site-footer__copyright">
-		<div class="wrap-logo">
-			<a href="https://votolegal.com.br" target="_blank" class="votolegal-logo"></a>
-		</div>
-		<div class="site-footer__copyright">
-      <p>
-        <a href="https://votolegal.com.br" target="_blank">Voto Legal</a> é uma plataforma de software livre homologada pelo TSE e opera sob o CNPJ "30.217.474/0001-50", razão social "Pague Junto - Tecnologia de Intermediação LTDA".
-        Todas as doações são registradas na blockchain da Decred com objetivo de garantir a autenticidade das transações, promovendo a confiabilidade no processo de doação eleitoral. Taxa de 7,4% via cartão de crédito ou 4% + R$4 via boleto, esse valor é destinado a taxas de operação financeira, sistemas de controle anti-fraude, impostos e infraestrutura.
-      </p>
-      <p>
-				<strong>© 2016-2018 <a href="https://votolegal.com.br" target="_blank">Voto Legal</a> • Uma iniciativa <a href="http://appcivico.com" target="_blank">AppCívico</a> - Tecnologias Cívicas • <a href="/termos.pdf" target="_blank">Contrato</a></strong>
-			</p>
-		</div>
+        <div class="wrap-logo">
+          <a href="https://votolegal.com.br" target="_blank" class="votolegal-logo"></a>
+        </div>
+        <div class="site-footer__copyright">
+          <p>
+            <a href="https://votolegal.com.br" target="_blank">Voto Legal</a> é uma plataforma de software livre homologada pelo TSE e opera sob o CNPJ "30.217.474/0001-50", razão social "Pague Junto - Tecnologia de Intermediação LTDA".
+            Todas as doações são registradas na blockchain da Decred com objetivo de garantir a autenticidade das transações, promovendo a confiabilidade no processo de doação eleitoral. Taxa de 7,4% via cartão de crédito ou 4% + R$4 via boleto, esse valor é destinado a taxas de operação financeira, sistemas de controle anti-fraude, impostos e infraestrutura.
+          </p>
+          <p>
+            <strong>© 2016-2018 <a href="https://votolegal.com.br" target="_blank">Voto Legal</a> • Uma iniciativa <a href="http://appcivico.com" target="_blank">AppCívico</a> - Tecnologias Cívicas • <a href="/termos.pdf" target="_blank">Contrato</a></strong>
+          </p>
+        </div>
       </div>
     </div>
   </footer>
