9f867f963c24677c844794ecda42ea43578e035e
Robson Sobral <robson@oficiodesign.com>
2018-07-17 15:53:39 -0300

    Fix credit card errors messages

--- src/components/steps/finalMessage.vue
+++ src/components/steps/finalMessage.vue
@@ -1,8 +1,7 @@
 <template>
   <section>
   <a class="donation-nav donation-nav--rewind" href="#" @click.prevent="goBack">voltar</a>
-  <div class="input-wrapper" v-html="messages[0].text"></div>
-  <p class="input-wrapper" v-html="messages[1].text"></p>
+    <div class="input-wrapper" v-for="(message, index) in messages" v-if="message.type == 'msg'" v-html="message.text" :key="index"></div>
   <ul id="wrap-share">
     <li>
       <a class="twitter" href="https://twitter.com/intent/tweet?text=Marina%20precisa%20de%20n%C3%B3s%20para%20equilibrar%20a%20disputa%21%20https%3A%2F%2Fdoemarina.com.br%3Fref%3Dagradecimento">Compartilhar</a>
@@ -11,9 +10,11 @@
       <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdoemarina.com.br%3Fref%3Dagradecimento&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore facebook">Compartilhar</a>
     </li>
   </ul>
-  <div class="donation-nav-wrap" v-if="messages[2]">
-    <a target="_blank" class="donation-nav donation-nav--forward" @click.prevent="redirectUser(messages[2])">{{ messages[2].text }} </a>
-  </div>
+    <div class="donation-nav-wrap">
+      <div class="input-wrapper" v-for="(message, index) in messages" v-if="message.type == 'link'" :key="index">
+        <a target="_blank" class="donation-nav donation-nav--block" :href="message.href" >{{ message.text }} </a>
+      </div>
+    </div>
   </section>
 </template>
 
