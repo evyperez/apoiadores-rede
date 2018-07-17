e7c463a33d07ff688091cea57ec7aca8bf9e962b
Robson Sobral <robson@oficiodesign.com>
2018-07-17 18:14:02 -0300

    Add notification component

---
+++ src/components/Notification.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="notification">
+    <div class="notification__title">
+      {{ title }}
+    </div>
+    <div class="notification__message">
+      {{ message }}
+    </div>
+    <div class="notification__footer">
+      #somosrede
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Notification',
+    props: {
+      title: String,
+      message: String,
+    }
+  }
+</script>
+
