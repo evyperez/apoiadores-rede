314a6afdd4539a8082354f0c399912c8e0a9a6f6
Robson Sobral <robson@oficiodesign.com>
2018-07-18 12:55:15 -0300

    Set notification transitions and animations

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -1,24 +1,50 @@
 <template>
-  <div class="notification">
-    <div class="notification__title">
-      {{ title }}
+  <transition name="toogle-donation">
+    <div class="notification" v-if="show">
+      <div class="notification__title">
+        {{ title }}
+      </div>
+      <div class="notification__message">
+        {{ message }}
+      </div>
+      <div class="notification__footer">
+        #somosrede
+      </div>
     </div>
-    <div class="notification__message">
-      {{ message }}
-    </div>
-    <div class="notification__footer">
-      #somosrede
-    </div>
-  </div>
+  </transition>
 </template>
 
 <script>
   export default {
     name: 'Notification',
+    data() {
+      return {
+        show: false,
+        showingNotification: 0,
+      }
+    },
     props: {
       title: String,
       message: String,
-    }
+      duration: {
+        default: 5000,
+        type: Number,
+      }
+    },
+    watch: {
+      title: function() {
+        this.$data.show = !this.$data.show;
+        this.$data.showingNotification = setTimeout(() => {
+          this.$data.show = !this.$data.show;
+        }, this.$props.duration);
+      },
+      message: function() {
+        this.$data.show = !this.$data.show;
+        this.$data.showingNotification = setTimeout(function() {
+          this.$data.show = !this.$data.show;
+        }, this.$props.duration);
+      },
+    },
   }
 </script>
 
