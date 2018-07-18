1c8b92afb05660be0c0cf931648c84a654e4db1f
Robson Sobral <robson@oficiodesign.com>
2018-07-18 16:15:17 -0300

    Change donation share buttons positions

--- src/components/steps/printBoleto.vue
+++ src/components/steps/printBoleto.vue
@@ -6,6 +6,12 @@
       </div>
       <div class="input-wrapper" v-for="(message, index) in messages" v-if="message.type == 'msg'" v-html="message.text" :key="index"></div>
 
+      <div class="donation-nav-wrap">
+        <div class="input-wrapper" v-for="(message, index) in messages" v-if="message.type == 'link'" :key="index">
+          <a target="_blank" class="donation-nav donation-nav--block" :href="message.href" >{{ message.text }} </a>
+        </div>
+      </div>
+
       <ul id="wrap-share">
         <li>
         <a class="twitter" href="https://twitter.com/intent/tweet?text=Marina%20precisa%20de%20n%C3%B3s%20para%20equilibrar%20a%20disputa%21%20https%3A%2F%2Fdoemarina.com.br%3Fref%3Dagradecimento">Compartilhar</a>
@@ -14,12 +20,6 @@
         <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdoemarina.com.br%3Fref%3Dagradecimento&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore facebook">Compartilhar</a>
         </li>
       </ul>
-
-      <div class="donation-nav-wrap">
-        <div class="input-wrapper" v-for="(message, index) in messages" v-if="message.type == 'link'" :key="index">
-          <a target="_blank" class="donation-nav donation-nav--block" :href="message.href" >{{ message.text }} </a>
-        </div>
-    </div>
     </section>
 </template>
 
