ba0bdc071ba6abb48ef5485d877c0f2c9a0548ae
Robson Sobral <robson@oficiodesign.com>
2018-07-25 13:24:28 -0300

    Add `paymentWaitingMessage` to state

--- src/store.js
+++ src/store.js
@@ -34,6 +34,7 @@ export default new Vuex.Store({
     recentDonation: {},
     address: {},
     paymentData: {},
+    paymentWatingMessage: '',
     hasMoreDonations: false,
     lastDonationMarker: '',
   },
